2f33d87cbad58ec406f4403598c66b5df6ecabb2
Bruno Sabio <bruno.sabio@hotmail.com>
2022-04-06 20:42:45 -0300

    feat:criando as rotas

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const path = require("path")
 const app = express()
+const port = 3000
 
-app.get('/', req,res) => res.sendFile(path.join(__dirname, ./viws))
+app.get('/', req,res) => res.sendFile(path.join(__dirname,.'views/home.html'))
 
-app.listen()
+app.listen(port, () => )
